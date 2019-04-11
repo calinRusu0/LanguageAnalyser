b05360505a0fad91a76d5bba7e3b8fd505cf7740
calinRusu0 <49458772+calinRusu0@users.noreply.github.com>
2019-04-11 23:01:55 +0100

    Add files via upload

---
+++ Control.java
@@ -0,0 +1,13 @@
+package analyser;
+
+/* Author: Calin Rusu
+   Student Number: C16443252
+   Program name: Language Analyser */
+public class Control 
+{
+
+	public static void main(String[] args)
+	{
+		screen myScreen = new screen("A Neat Language Analyser by Calin Rusu.");
+	}
+}
